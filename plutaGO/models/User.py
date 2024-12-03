af81279a738a2c68828ac08656d6e506a2e5a1e7
Nikodem Młyński <nikodemmlynski08@gmail.com>
2024-12-03 14:26:01 +0100

    add 'amounts_of_pluts' field into user table it is like user balance and will be useful in making payments

--- plutaGO/models/User.py
+++ plutaGO/models/User.py
@@ -1,10 +1,11 @@
 
 class User:
-    def __init__(self, id, name, surname, email, password, role):
+    def __init__(self, id, name, surname, email, password, role, amount_of_pluts):
         self.id = id
         self.name = name
         self.surname = surname
         self.email = email
         self.password = password
         self.role = role
+        self.amount_of_pluts = amount_of_pluts
 
